eff7f4a315e0005ab52d5649a830f856cee41232
armanmass <armanmass2002@gmail.com>
2025-07-13 04:47:38 -0700

    started typed bst

--- hashtable.cpp
+++ hashtable.cpp
@@ -83,23 +83,14 @@ class HashTable {
 };
 
 int main(){
-    HashTable<int,string> h;
+    HashTable<string,string> h;
     if(h.empty()){
         cout << "HashTable constructed and empty." << endl;
     }else{
         cout << "Incorrect implementation for HT or empty() member." << endl;
     }
-    h.insert(13242,"test");
-    h.remove(69);
-    h.insert(231,"test2");
-    h.insert(15234,"test3");
-    h.insert(1234,"test4");
-    h.insert(112367,"test5");
-    h.insert(1,"1");
-    h.insert(1,"2");
-    h.remove(1234);
-    h.remove(1234);
-    h.remove(1234);
+
+    h.insert("test","test");
 
     cout << h.size() << " elements in table." << endl;
 
